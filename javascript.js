2ac5c2c3d32e720ffb7a83abe6d783654f0fd973
louis-hibon <100513482+louis-hibon@users.noreply.github.com>
2022-04-26 18:22:50 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+$(window).load(function () {
+    $('.trigger_popup_fricc_deezer').click(function(){
+       $('.hover_bkgr_fricc_deezer').show();
+    });
+    $('.hover_bkgr_fricc_deezer').click(function(){
+        $('.hover_bkgr_fricc_deezer').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc_deezer').hide();
+    });
+});
+$(window).load(function () {
+     $('.trigger_popup_fricc_youtube').click(function(){
+       $('.hover_bkgr_fricc_youtube').show();
+    });
+    $('.hover_bkgr_fricc_youtube').click(function(){
+        $('.hover_bkgr_fricc_youtube').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc_youtube').hide();
+    });
+});
